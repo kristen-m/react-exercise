645fb90fee15454603759061acb0fdb712f691a7
kristenmason <kristen-m@users.noreply.github.com>
2021-09-20 16:13:52 -0700

    Lab exercise from class

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+let count = 0;
+function render() {
+    count++;
+    const listContents = [
+        {
+            id: 512,
+            name: "Neil Rhodes",
+            email: "rhodes@hmc.edu",
+            phone: "(909) 555-1212"
+        },
+        {
+            id: 787,
+            name: "Barack Obama",
+            email: "ex-prez@whitehouse.gov",
+            phone: "(312) 555-1212"
+        }
+    ];
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    console.log(listContents);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const listItems = listContents.map(
+        e => <li key={e.id}> <div id="name"> {e.name} </div> </li>,
+        b => <li key={b.id}><div id="email"> {b.email} </div></li>,
+        c => <li key={c.id}><div id="phone"> {c.phone} </div> </li>);
+
+
+    ReactDOM.render(
+        <div>
+            <ul>
+                {listItems}
+            </ul>
+        </div>,
+        document.getElementById('root')
+    );
+}
+
+render();
+setInterval(render, 2000);
